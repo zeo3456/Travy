5802b401b4fe7f02f9d4f84f3c6f39399e81c47c
Zhantao Xu <xzt1994@gmail.com>
2015-05-08 19:28:15 -0500

    v 0.3

--- app/src/main/java/com/example/travy/MapFragmentTab.java
+++ app/src/main/java/com/example/travy/MapFragmentTab.java
@@ -1,6 +1,7 @@
 package com.example.travy;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,21 +20,17 @@ public class MapFragmentTab extends Fragment {
     private static GoogleMap mMap;
     private static Double latitude, longitude;
 
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         if (container == null) {
             return null;
         }
         view = (RelativeLayout) inflater.inflate(R.layout.fragment_map, container, false);
-        latitude = 43.084994;
-        longitude = 89.400412;
+        latitude = 43.077047;
+        longitude = -89.401225;;
         setUpMapIfNeeded(); // For setting up the MapFragment
         return view;
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
     }
 
     public static void setUpMapIfNeeded() {
@@ -47,13 +44,6 @@ public class MapFragmentTab extends Fragment {
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the
-     * camera.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap}
-     * is not null.
-     */
     private static void setUpMap() {
         // For showing a move to my loction button
         mMap.setMyLocationEnabled(true);
@@ -63,4 +53,36 @@ public class MapFragmentTab extends Fragment {
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 12.0f));
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        if (mMap != null)
+            setUpMap();
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) MainActivity.fragmentManager
+                    .findFragmentById(R.id.map)).getMap(); // getMap is deprecated
+            // Check if we were successful in obtaining the map.
+            if (mMap != null)
+                setUpMap();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) MainActivity.fragmentManager
+                    .findFragmentById(R.id.map)).getMap();
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mMap != null) {
+            MainActivity.fragmentManager.beginTransaction()
+                    .remove(MainActivity.fragmentManager.findFragmentById(R.id.map)).commit();
+            mMap = null;
+        }
+    }
 }
